dc2845d0ca88fdc162d5d8d0eef9cdbcb4714a9d
Matias Schroder <matischroder99@gmail.com>
2024-12-16 18:52:25 +1000

    update training configuration

--- bot/train.py
+++ bot/train.py
@@ -1,24 +1,27 @@
-import openai
 import os
+import nest_asyncio
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
-import faiss
+# Apply nest_asyncio to allow nested event loops
+nest_asyncio.apply()
 
-
-from llama_index import (
-    SimpleDirectoryReader,
-    VectorStoreIndex,
-    StorageContext,
-)
-from llama_index.vector_stores.faiss import FaissVectorStore
-
-openai.api_key = os.environ.get("OPENAI_API_KEY")
-
-d = 1536
-faiss_index = faiss.IndexFlatL2(d)
+# Define a local Hugging Face embedding model
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
 
 def train_bot():
     documents = SimpleDirectoryReader("./data/md", exclude=["**/*.png"]).load_data()
-    index = VectorStoreIndex.from_documents(documents)
+    print("llego 1")
+    print(documents)
+    index = VectorStoreIndex.from_documents(
+        documents, show_progress=True, use_async=True, embed_model=embed_model
+    )
+    print("llego 2")
     index.set_index_id("vector_index")
+    print("llego 3")
     index.storage_context.persist("./data/storage")
+    print("llego 4")
+
+
+train_bot()
